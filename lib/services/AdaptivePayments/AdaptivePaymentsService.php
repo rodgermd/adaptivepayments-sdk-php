2758b28eb9350ecef823e7024769dfa7452dab73
lathavairamani <lvairamani@paypal.com>
2013-01-29 16:16:21 +0530

    updated stubs for composer

--- lib/services/AdaptivePayments/AdaptivePaymentsService.php
+++ lib/services/AdaptivePayments/AdaptivePaymentsService.php
@@ -12,13 +12,15 @@ class AdaptivePaymentsService extends PPBaseService {
 	private static $SERVICE_NAME = "AdaptivePayments";
 
     // SDK Name
-	protected static $SDK_NAME = "adaptivepayments-php-sdk";
+	protected static $SDK_NAME = "sdkname";
 	
 	// SDK Version
-	protected static $SDK_VERSION = "2.1.96";
+	protected static $SDK_VERSION = "sdkversion";
 
 	public function __construct() {
 		parent::__construct(self::$SERVICE_NAME, 'NV', array('PPPlatformServiceHandler'));
+        parent::$SDK_NAME    = self::$SDK_NAME ;
+        parent::$SDK_VERSION = self::$SDK_VERSION;
 	}
 
 
